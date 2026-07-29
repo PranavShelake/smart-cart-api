5ddfe52f41445df7b5e3c74be8bd234f1165aa3c
PranavShelake <pranavshelake99@gmail.com>
2026-07-29 12:04:43 +0530

    Remove frontend subtree import

--- frontend/src/utils/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-// src/utils/formatCurrency.ts
-export const formatCurrency = (amount: number): string =>
-  new Intl.NumberFormat('en-IN', {
-    style:              'currency',
-    currency:           'INR',
-    maximumFractionDigits: 0,
-  }).format(amount)
-
